a69abfb965e00cd869f9db9c1458e5f559d349d0
Yang Sun <s27y@hotmail.com>
2015-04-12 13:14:55 +0100

    refactoring

--- src/SpaceInvaders.java
+++ src/SpaceInvaders.java
@@ -1,4 +1,3 @@
-
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
@@ -11,15 +10,33 @@ import java.util.Random;
 import javax.swing.JApplet;
 import javax.swing.JPanel;
 
+enum GameState {
+	INIT, FIRED, HITED, FINISHED;
+}
+
 public class SpaceInvaders extends JApplet {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 3212324234L;
-	public final int STAT_INIT = 1;
-	public final int STAT_FIRED = 2;
-	public final int STAT_HITED = 3;
-	public final int STAT_FINISHED = 4;
+	private final int SCREEN_SIZE = 400;
+	private final int ENMEY_SIZE= 20;
+	private final int LAUNCHER_SIZE= 20;
+	private final int BULLET_SPEED = 1;
+	private final int SCREEN_REFRESH_RATE = 24;	
+	private final int ENMEY_OFFSET = 20;  //density of enmey
+	
+	private final String IMG_BOOM_RED = "images/boom_red.jpg";
+	private final String IMG_BULLET_RED = "images/bullet_red.jpg";
+	private final String IMG_ENMEY_WHITE = "images/enmey_white.jpg";
+	private final String IMG_GAMEOVER = "images/gameover.jpg";
+	private final String IMG_LAUNCHER_GREEN = "images/launcher_green.jpg";
+	private final String IMG_LV_UP = "images/lv_up.jpg";
+	private final String IMG_START = "images/start.jpg";
+	
+	
+	
+	private final int NUMBER_OF_ENMEY = 10;
 
 	private static int showRandomInteger(int aStart, int aEnd, Random aRandom) {
 		if (aStart > aEnd) {
@@ -33,72 +50,74 @@ public class SpaceInvaders extends JApplet {
 		return randomNumber;
 	}
 
-	private static ArrayList<Integer> listRandomInt(int min, int max, int amount) {
-		ArrayList<Integer> al = new ArrayList<Integer>();
-		Random r = new Random();
-		int rint = showRandomInteger(min, max, r);
-		al.add(rint);
+	private static ArrayList<Integer> listRandomInt(int min, int max, int numOfInt, int offset) {
+		ArrayList<Integer> randomIntList = new ArrayList<Integer>();
+		Random randomGenerator = new Random();
+		int randomInt = showRandomInteger(min, max, randomGenerator);
+		randomIntList.add(randomInt);
 
-		while (al.size() <= amount) {
+		while (randomIntList.size() <= numOfInt) {
 			int count = 0;
-			rint = showRandomInteger(min, max, r);
-			for (int i = 0; i < al.size(); i++) {
-				if (rint < al.get(i) - 20 || rint > al.get(i) + 20) {
+			randomInt = showRandomInteger(min, max, randomGenerator);
+			
+			//check new int is within offset
+			for (int i = 0; i < randomIntList.size(); i++) {
+				if (randomInt < randomIntList.get(i) - offset
+						|| randomInt > randomIntList.get(i) + offset) {
 					count++;
 				}
 			}
-			if (count == al.size()) {
-				al.add(rint);
+			if (count == randomIntList.size()) {
+				randomIntList.add(randomInt);
 			}
 		}
-		return al;
+		return randomIntList;
 	}
 
 	public class EnemyThread extends Thread {
 		int x_position = 0;
 		int y_position = 0;
 		int size;
-		int m_stat;
+		GameState state;
 
 		public EnemyThread(int x, int y) {
 			this.x_position = x;
 			this.y_position = y;
-			this.size = 20;
-			this.m_stat = STAT_INIT;
-		}
-
-		public EnemyThread(int x, int y, int s) {
-			this.x_position = x;
-			this.y_position = y;
-			this.size = s;
-			this.m_stat = STAT_INIT;
+			this.size = ENMEY_SIZE;
+			this.state = GameState.INIT;
 		}
 
 		public void run() {
-			this.m_stat = STAT_FIRED;
+			this.state = GameState.FIRED;
 			try {
-				while (!interrupted() && this.m_stat != STAT_FINISHED) {
-					switch (this.m_stat) {
-					case STAT_HITED:
-						for (int i = 0; i <= 2; i++) {
+				while (!interrupted() && this.state != GameState.FINISHED) {
+					switch (this.state) {
+					case HITED:
+						for (int i = 0; i < 2; i++) {
+							// hitted enmey goes backward 2 times
 							this.y_position--;
 							sleep(300);
 						}
-						this.m_stat = STAT_FINISHED;
+						this.state = GameState.FINISHED;
 						break;
-					case STAT_FIRED:
+					case FIRED:
 						this.y_position++;
-						sleep(100); // 10/1000th = 1/100th second
+						sleep(100); // 100ms
+						break;
+					default:
 						break;
 					}
-
 				}
 			} catch (InterruptedException e) {
 			}
 		}
 
 	}
-
+	/**
+	 * Thread for refresh screen
+	 * @author yangsun
+	 *
+	 */
 	public class TimerThread extends Thread {
 		int Time = 0;
 
@@ -110,8 +129,7 @@ public class SpaceInvaders extends JApplet {
 		public void run() {
 			try {
 				while (!interrupted()) {
-					sleep(this.Time); // 10/1000th = 1/100th second
-					// hitChecker();
+					sleep(this.Time); 
 					repaint();
 				}
 			} catch (InterruptedException e) {
@@ -122,24 +140,23 @@ public class SpaceInvaders extends JApplet {
 	public class BulletThread extends Thread {
 		int x_position = 0;
 		int y_position = 0;
-		int m_stat;
+		GameState m_stat;
 
 		public BulletThread(int x, int y) {
-			this.x_position = x + 10;
-			// -20 for launcher height
-			this.y_position = y - 20;
-			this.m_stat = STAT_INIT;
+			this.x_position = x + LAUNCHER_SIZE/2;
+			this.y_position = y - LAUNCHER_SIZE;
+			this.m_stat = GameState.INIT;
 		}
 
 		@Override
 		public void run() {
-			this.m_stat = STAT_FIRED;
+			this.m_stat = GameState.FIRED;
 			try {
 				while (!interrupted() && y_position > 10) {
-					y_position--;
-					sleep(10); // 10/1000th = 1/100th second
+					y_position-=BULLET_SPEED;
+					sleep(10); // 10ms
 				}
-				this.m_stat = STAT_FINISHED;
+				this.m_stat = GameState.FINISHED;
 			} catch (InterruptedException e) {
 			}
 		}
@@ -147,7 +164,7 @@ public class SpaceInvaders extends JApplet {
 
 	@Override
 	public void init() {
-		this.setSize(400, 400);
+		this.setSize(SCREEN_SIZE, SCREEN_SIZE);
 		setContentPane(new ContentPane());
 
 	}
@@ -159,45 +176,44 @@ public class SpaceInvaders extends JApplet {
 		private static final long serialVersionUID = -3905609900670542128L;
 		private boolean isStarted = false;
 		private Image m_launcher_img, m_em_img, m_lvup_img, m_start_img,
-				m_hited_img, m_blt_img,m_gameover_img;
+				m_hited_img, m_blt_img, m_gameover_img;
 
 		private int m_x = 100; // Initial coordinates of launcher
 
 		ArrayList<BulletThread> bltList = new ArrayList<BulletThread>();
 		ArrayList<EnemyThread> emList = new ArrayList<EnemyThread>();
 
-		TimerThread tt = new TimerThread(1000 / 24);
-		boolean isGameOver=false;
+		TimerThread tt = new TimerThread(1000 / SCREEN_REFRESH_RATE);
+		boolean isGameOver = false;
+
 		public ContentPane() {
 			super();
 
 			setBackground(Color.BLACK);
 
-			m_launcher_img = getImage(getDocumentBase(),
-					"images/launcher_green.jpg");
-			m_em_img = getImage(getDocumentBase(), "images/enmey_white.jpg");
-			m_lvup_img = getImage(getDocumentBase(), "images/lv_up.jpg");
-			m_start_img = getImage(getDocumentBase(), "images/start.jpg");
-			m_hited_img = getImage(getDocumentBase(), "images/boom_red.jpg");
-			m_blt_img = getImage(getDocumentBase(), "images/bullet_red.jpg");
-			m_gameover_img  = getImage(getDocumentBase(), "images/gameover.jpg");
+			m_launcher_img = getImage(getDocumentBase(),IMG_LAUNCHER_GREEN);
+			m_em_img = getImage(getDocumentBase(), IMG_ENMEY_WHITE);
+			m_lvup_img = getImage(getDocumentBase(), IMG_LV_UP);
+			m_start_img = getImage(getDocumentBase(), IMG_START);
+			m_hited_img = getImage(getDocumentBase(), IMG_BOOM_RED);
+			m_blt_img = getImage(getDocumentBase(), IMG_BULLET_RED);
+			m_gameover_img = getImage(getDocumentBase(), IMG_GAMEOVER);
 			// addKeyListener() and setFocusable() must both be included in
 			// order to allow key input
 			addKeyListener(this);
 			setFocusable(true);
 		}
 
-		// creat enemies
+		// create enemies
 		public void initEnemy() {
-			Random r;
-			ArrayList<Integer> al = listRandomInt(10, 390, 10);
+
+			ArrayList<Integer> al = listRandomInt(ENMEY_SIZE/2, SCREEN_SIZE-ENMEY_SIZE/2, NUMBER_OF_ENMEY, ENMEY_OFFSET);
 			for (int i : al) {
 				emList.add(new EnemyThread(i, 20));
 			}
 
 			for (int j = 0; j < 10; j++) {
-				r = new Random();
-				al = listRandomInt(10, 390, 10);
+				al = listRandomInt(10, 390, 10, 20);
 				for (int i : al) {
 					emList.add(new EnemyThread(i, -20 * j));
 				}
@@ -209,16 +225,16 @@ public class SpaceInvaders extends JApplet {
 				ArrayList<EnemyThread> etList) {
 			outter: for (BulletThread bt : btList) {
 				// bullet is finished?
-				if (bt.m_stat == STAT_FIRED) {
+				if (bt.m_stat == GameState.FIRED) {
 					for (EnemyThread et : etList) {
-						// enemy is hited?
-						if (et.m_stat == STAT_FIRED
+						// enemy is hitted?
+						if (et.state == GameState.FIRED
 								&& (bt.x_position <= et.x_position + et.size
 										- 0 && bt.x_position >= et.x_position - 0)
 								&& (bt.y_position >= et.y_position && bt.y_position <= et.y_position
 										+ et.size)) {
-							bt.m_stat = STAT_FINISHED;
-							et.m_stat = STAT_HITED;
+							bt.m_stat = GameState.FINISHED;
+							et.state = GameState.HITED;
 
 							continue outter;
 						}
@@ -230,7 +246,7 @@ public class SpaceInvaders extends JApplet {
 		public int hitedEnemy(ArrayList<EnemyThread> etList) {
 			int count = 0;
 			for (EnemyThread et : etList) {
-				if (et.m_stat == STAT_FINISHED)
+				if (et.state == GameState.FINISHED)
 					count++;
 			}
 			return count;
@@ -239,21 +255,19 @@ public class SpaceInvaders extends JApplet {
 		@Override
 		public void paintComponent(Graphics g) {
 			super.paintComponent(g);
-			
-			Font m_smallFont,
-	           m_bigFont;
+
+			Font m_smallFont, m_bigFont;
 			m_bigFont = new Font("Times New Roman", Font.BOLD, 50);
-			m_smallFont= new Font("Times New Roman", Font.BOLD, 15);
+			m_smallFont = new Font("Times New Roman", Font.BOLD, 15);
 			g.setFont(m_smallFont);
-			
-			for(EnemyThread et: emList)
-			{
-				if(et.y_position>this.getHeight()-50&&et.m_stat==STAT_FIRED)
-				{
-					isGameOver=true;
+
+			for (EnemyThread et : emList) {
+				if (et.y_position > this.getHeight() - 50
+						&& et.state == GameState.FIRED) {
+					isGameOver = true;
 				}
 			}
-			
+
 			if (hitedEnemy(emList) == emList.size() && emList.size() != 0) {
 				g.drawImage(m_lvup_img, 0, 0, getWidth(), getHeight(), this);
 			} else if (this.isStarted == false) {
@@ -261,10 +275,11 @@ public class SpaceInvaders extends JApplet {
 			} else if (this.isGameOver == true) {
 				g.setFont(m_bigFont);
 				g.drawImage(m_gameover_img, 0, 0, getWidth(), getHeight(), this);
-				
-				g.drawString("Score: "+this.hitedEnemy(emList), this.getWidth()/2-80, 350);
+
+				g.drawString("Score: " + this.hitedEnemy(emList),
+						this.getWidth() / 2 - 80, 350);
 			}
-			
+
 			else {
 
 				g.setColor(Color.white);
@@ -275,14 +290,15 @@ public class SpaceInvaders extends JApplet {
 				hitChecker(bltList, emList);
 
 				// how many bullets have been fired
-				
-				g.drawString("Missile Fired: "+bltList.size(), this.getWidth()-120, 10);
+
+				g.drawString("Missile Fired: " + bltList.size(),
+						this.getWidth() - 120, 10);
 				g.drawString("Score:" + this.hitedEnemy(emList), 10, 10);
 
 				// g.drawString("^", m_x, this.getHeight());
-				
+
 				for (EnemyThread et : emList) {
-					if (et.m_stat == STAT_HITED) {
+					if (et.state == GameState.HITED) {
 						int width1 = m_hited_img.getWidth(this), height1 = m_hited_img
 								.getHeight(this);
 
@@ -290,21 +306,20 @@ public class SpaceInvaders extends JApplet {
 								width1, height1, this);
 					}
 
-					else if (et.m_stat == STAT_FIRED) {
+					else if (et.state == GameState.FIRED) {
 						int width1 = m_em_img.getWidth(this), height1 = m_em_img
 								.getHeight(this);
 						// g.drawString("*", et.x, et.y);
-						if(et.y_position>15)
-						{
+						if (et.y_position > 15) {
 							g.drawImage(m_em_img, et.x_position, et.y_position,
 									width1, height1, this);
 						}
-						
+
 					}
 
 				}
 				for (BulletThread bt : bltList) {
-					if (bt.m_stat == STAT_FIRED)
+					if (bt.m_stat == GameState.FIRED)
 						g.drawImage(m_blt_img, bt.x_position, bt.y_position,
 								this);
 
